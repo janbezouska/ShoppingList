e5c6c5cc46456f2ad82a955e7f8d39fef3fb7aa2
janbezouska <bezouska.jan.2018@skola.ssps.cz>
2021-03-01 12:48:03 +0100

    funkční verze

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,60 @@ namespace ShoppingList
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.butAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // butAdd
+            // 
+            this.butAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.butAdd.Location = new System.Drawing.Point(15, 35);
+            this.butAdd.Name = "butAdd";
+            this.butAdd.Size = new System.Drawing.Size(31, 32);
+            this.butAdd.TabIndex = 1;
+            this.butAdd.Text = "+";
+            this.butAdd.UseVisualStyleBackColor = true;
+            this.butAdd.Click += new System.EventHandler(this.ButAdd_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nakoupit:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(236, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Hotovo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(362, 309);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.butAdd);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button butAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,101 @@ namespace ShoppingList
 {
     public partial class Form1 : Form
     {
+        public List<Button> buttons = new List<Button>();
+        public List<TextBox> textBoxes = new List<TextBox>();
+        public List<CheckBox> checkBoxes = new List<CheckBox>();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ButAdd_Click(object sender, EventArgs e)
+        {
+            butAdd.Top += 25;
+
+            this.Controls.Add(CreateButton());
+            this.Controls.Add(CreateTextBox());
+            this.Controls.Add(CreateCheckBox());
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            for (int i = 0; i < buttons.Count; i++)
+            {
+                if(int.Parse(buttons[i].Name.Remove(0,3)) == int.Parse(button.Name.Remove(0, 3)))
+                {
+                    this.Controls.Remove(buttons[i]);
+                    this.Controls.Remove(textBoxes[i]);
+                    this.Controls.Remove(checkBoxes[i]);
+
+                    for (int f = i+1; f < buttons.Count; f++)
+                    {
+                        buttons[f].Top -= 25;
+                        textBoxes[f].Top -= 25;
+                        checkBoxes[f].Top -= 25;
+                    }
+                    butAdd.Top -= 25;
+
+                    buttons.Remove(buttons[i]);
+                    textBoxes.Remove(textBoxes[i]);
+                    checkBoxes.Remove(checkBoxes[i]);
+                }
+            }
+        }
+
+        public Button CreateButton()
+        {
+            Button button = new Button();
+
+            button.Left = this.Width - 100;
+            if (buttons.Count == 0)
+                button.Top = 30;
+            else
+                button.Top = buttons[buttons.Count - 1].Top + 25;
+
+            buttons.Add(button);
+
+            button.Name = "but" + Convert.ToString(buttons.Count - 1);
+            button.Text = "Smazat";
+            button.Click += Button_Click;
+
+            return button;
+        }
+
+        public TextBox CreateTextBox()
+        {
+            TextBox textBox = new TextBox();
+
+            textBox.Left = 15;
+            if (textBoxes.Count == 0)
+                textBox.Top = 30;
+            else
+                textBox.Top = textBoxes[textBoxes.Count - 1].Top + 25;
+
+            textBox.Name = "txt" + Convert.ToString(buttons.Count - 1);
+            textBox.Width = 220;
+
+            textBoxes.Add(textBox);
+            return textBox;
+        }
+
+        public CheckBox CreateCheckBox()
+        {
+            CheckBox checkBox = new CheckBox();
+
+            checkBox.Left = this.Width - 130;
+            if (checkBoxes.Count == 0)
+                checkBox.Top = 30;
+            else
+                checkBox.Top = checkBoxes[checkBoxes.Count - 1].Top + 25;
+
+            checkBox.Name = "chb" + Convert.ToString(buttons.Count - 1);
+
+            checkBoxes.Add(checkBox);
+            return checkBox;
         }
     }
 }
